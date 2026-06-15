72fc06f69561baeee599f585228e5e7655cea5b3
keshava8769 <bairuchenna@gmail.com>
2026-06-16 00:38:55 +0530

    Auto create DB tables on startup

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 from app import create_app, db
-from flask import redirect, url_for
 
 app = create_app()
 
-@app.route('/')
-def home():
-    return redirect(url_for('jobs.list_jobs'))
+# Auto-create all tables when app starts
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run(debug=False)
