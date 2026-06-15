6ee67cdd8b521d4f9b84965b92522af9bd1fff5b
keshava8769 <bairuchenna@gmail.com>
2026-06-16 00:42:17 +0530

    Fix root route redirect

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from app import create_app, db
+from flask import redirect, url_for
 
 app = create_app()
 
 # Auto-create all tables when app starts
 with app.app_context():
     db.create_all()
+
+@app.route('/')
+def home():
+    return redirect(url_for('jobs.list_jobs'))
 
 if __name__ == '__main__':
     app.run(debug=False)
